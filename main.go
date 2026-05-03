9e1c239afb3c9f7132d06da7964ce9c5ceea4e44
stricklandrw <stricklandrw@gmail.com>
2026-05-02 21:10:40 -0400

    Ch1: Servers, L11: Custom Handlers - Complete

--- main.go
+++ main.go
@@ -10,7 +10,16 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+
+	mux.Handle("/app", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+
+	mux.HandleFunc("/app/assets", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Add("Content-Type", "text/html; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("<html><pre>\n<a href=\"assets/logo.png\">logo.png</a>\n</pre></html>"))
+	})
+
+	mux.HandleFunc("/healthz", handlerReadiness)
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -19,4 +28,10 @@ func main() {
 
 	log.Printf("Serving files from %s on port %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
