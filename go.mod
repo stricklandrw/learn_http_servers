42732a18d444c7c5da42444f7ed21e93e0466db8
stricklandrw <stricklandrw@gmail.com>
2026-05-06 16:24:43 -0400

    Ch5: Storage, L3: SQLC complete

---
+++ go.mod
@@ -0,0 +1,9 @@
+module learn_http_servers
+
+go 1.26.2
+
+require (
+	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.12.3
+)
--- main.go
+++ main.go
@@ -1,22 +1,46 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+
+	"os"
 	"sync/atomic"
+
+	"github.com/joho/godotenv"
+
+	"learn_http_servers/internal/database"
+
+	_ "github.com/lib/pq"
 )
 
 // struct to hold fileserver hit count
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db             *database.Queries
 }
 
 func main() {
+	godotenv.Load()
+
 	const filepathRoot = "."
 	const port = "8080"
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL environment variable is not set")
+	}
+
+	dbConn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error opening database: %s", err)
+	}
+
+	dbQueries := database.New(dbConn)
 
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		db:             dbQueries,
 	}
 
 	mux := http.NewServeMux()
