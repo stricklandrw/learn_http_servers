23c4eaf442710e8ece46fcd58f8958a8bd690ff7
stricklandrw <stricklandrw@gmail.com>
2026-05-06 10:06:57 -0400

    Ch5: Storage, L2: Goose Migrations complete

---
+++ sql/schema/001_users.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+CREATE TABLE
+users (
+    id INTEGER PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    email TEXT NOT NULL UNIQUE
+);
+
+-- +goose Down
+DROP TABLE users;
