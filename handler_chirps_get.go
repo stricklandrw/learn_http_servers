68d42510ff2f677082aedb511f52853297d0e543
stricklandrw <stricklandrw@gmail.com>
2026-06-29 14:07:48 -0400

    Ch9: Documentation, L1: Documentation - Working

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -1,14 +1,38 @@
 package main
 
 import (
+	"learn_http_servers/internal/database"
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
-	dbChirps, err := cfg.db.GetAllChirps(r.Context())
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't get chirps", err)
-		return
+	var authorUUID uuid.UUID
+	var err error
+
+	author := r.URL.Query().Get("author_id")
+	dbChirps := []database.Chirp{}
+
+	if author != "" {
+		authorUUID, err = uuid.Parse(author)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid author ID", err)
+			return
+		}
+	}
+	if authorUUID != uuid.Nil {
+		dbChirps, err = cfg.db.GetChirpsByUserID(r.Context(), authorUUID)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Couldn't get chirps by user ID", err)
+			return
+		}
+	} else {
+		dbChirps, err = cfg.db.GetAllChirps(r.Context())
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Couldn't get chirps", err)
+			return
+		}
 	}
 
 	chirps := []Chirp{}
--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -17,6 +17,11 @@ ORDER BY created_at ASC;
 SELECT * FROM chirps
 WHERE id = $1;
 
+-- name: GetChirpsByUserID :many
+SELECT * FROM chirps
+WHERE user_id = $1
+ORDER BY created_at ASC;
+
 -- name: DeleteChirp :exec
 DELETE FROM chirps
 WHERE id = $1;
