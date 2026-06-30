97cec53ee7f8810aab849ab48b8e88396e8eeb03
stricklandrw <stricklandrw@gmail.com>
2026-06-30 11:01:06 -0400

    Ch9: Documentation, L3: Sorting Chips - Complete

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -3,6 +3,7 @@ package main
 import (
 	"learn_http_servers/internal/database"
 	"net/http"
+	"sort"
 
 	"github.com/google/uuid"
 )
@@ -46,6 +47,12 @@ func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
 			UpdatedAt: chirp.UpdatedAt,
 			Body:      chirp.Body,
 			UserID:    chirp.UserID,
+		})
+	}
+
+	if r.URL.Query().Get("sort") == "desc" {
+		sort.Slice(chirps, func(i, j int) bool {
+			return chirps[i].CreatedAt.After(chirps[j].CreatedAt)
 		})
 	}
 
